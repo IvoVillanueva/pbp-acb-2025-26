eb08094c55633dd19afeee6bfe64331ebdbad049
IvoVillanueva <56939175+IvoVillanueva@users.noreply.github.com>
2025-11-09 21:24:02 +0100

    creacion del archivo resultados

--- scripts/resultados.R
+++ scripts/resultados.R
@@ -8,7 +8,7 @@ ids <- read_csv("https://raw.githubusercontent.com/IvoVillanueva/pbp-acb-2025-26
 
 
 id <- ids %>% 
-  select(id, date = start_date) %>% 
+  select(id, date = end_date) %>% 
   mutate(
     date = as.Date(as_datetime(date))
   ) %>%
@@ -16,9 +16,9 @@ id <- ids %>%
     date <=today()) %>% 
   pull(id)
 
-id <- 5886
+
 iddf <- function(id) {
-  link <- paste0("https://api2.acb.com/api/v1/openapilive/Matches/matchesbymatchweeklite?idCompetition=1&idEdition=90&idMatchweek=", id)
+  link <- paste0(Sys.getenv("API_RESULTADOS"), id)
   res1 <- GET(url = link, add_headers(.headers = headers))
   json_resp1 <- fromJSON(content(res1, "text"))
   matches <- pluck(json_resp1) %>%
@@ -30,9 +30,48 @@ iddf <- function(id) {
 
 resultados <- map_df(id, iddf)
 
-resultados %>%
+victorias_derrotas <- resultados %>%
   select(
        abb_local = local_team_team_abbrev_name,
        score_local,
        abb_visitor = visitor_team_team_abbrev_name,
-       score_visitor) 
+       score_visitor,
+       jornada = matchweek_descriptor) %>% 
+  mutate(
+    ganador = if_else(score_local > score_visitor, abb_local, abb_visitor)
+  ) %>%
+  pivot_longer(
+    cols = c(abb_local, abb_visitor),
+    names_to = "rol",
+    values_to = "abb"
+  ) %>%
+  mutate(
+    resultado = if_else(abb == ganador, "W", "L")  # W = victoria, L = derrota
+  ) %>%
+  select(abb, resultado, jornada)
+
+rachas_actual <-victorias_derrotas %>%
+  mutate(jornada = as.numeric(str_remove(jornada, "Jornada "))) %>% 
+  arrange(abb, jornada) %>%
+  group_by(abb) %>%
+  mutate(
+    ultimo = last(resultado)
+  ) %>%
+  arrange(desc(jornada), .by_group = TRUE) %>%
+  mutate(
+    sigue_igual = resultado == ultimo,
+    flag = cumall(sigue_igual)
+  ) %>%
+  summarise(
+    tipo_racha = first(ultimo),
+    racha_actual = sum(flag),
+    .groups = "drop"
+  ) %>%
+  arrange(desc(tipo_racha), desc(racha_actual)) %>% 
+  mutate(win_lose = paste0(tipo_racha, racha_actual)) %>% 
+  select(abb, win_lose)
+
+
+# write dataframe to .csv in a folder called "data/"
+#write.csv(rachas_actual, "data/racha_2025_26.csv", row.names = FALSE)
+

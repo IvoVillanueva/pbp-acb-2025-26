8d6f9d1dce91891289d8aafeaa526e883dc8ace9
Ivan <ivan@Ivan-MacBook-Air.local>
2025-11-09 21:36:50 +0100

    embellecer archivo

--- scripts/resultados.R
+++ scripts/resultados.R
@@ -3,40 +3,43 @@ source("scripts/helpers.R")
 # Create data directory if it doesn't exist
 if (!dir.exists("data")) dir.create("data")
 
-
+# csv con los datos
 ids <- read_csv("https://raw.githubusercontent.com/IvoVillanueva/pbp-acb-2025-26/refs/heads/main/calendario/ids_df.csv")
 
-
-id <- ids %>% 
-  select(id, date = end_date) %>% 
+# filtro para las semanas
+id <- ids %>%
+  select(id, date = end_date) %>%
   mutate(
     date = as.Date(as_datetime(date))
   ) %>%
   filter(
-    date <=today()) %>% 
+    date <= today()
+  ) %>%
   pull(id)
 
-
+# funcion resultados
 iddf <- function(id) {
   link <- paste0(Sys.getenv("API_RESULTADOS"), id)
   res1 <- GET(url = link, add_headers(.headers = headers))
   json_resp1 <- fromJSON(content(res1, "text"))
   matches <- pluck(json_resp1) %>%
-    unnest(cols = c(competition, phase, local_team, visitor_team, arena, edition), names_sep = "_")
-
-
-  return(matches)
+    unnest(cols = c(
+      competition, phase, local_team,
+      visitor_team, arena, edition
+    ), names_sep = "_")
 }
 
 resultados <- map_df(id, iddf)
 
+# calcular victorias derrotas
 victorias_derrotas <- resultados %>%
   select(
-       abb_local = local_team_team_abbrev_name,
-       score_local,
-       abb_visitor = visitor_team_team_abbrev_name,
-       score_visitor,
-       jornada = matchweek_descriptor) %>% 
+    abb_local = local_team_team_abbrev_name,
+    score_local,
+    abb_visitor = visitor_team_team_abbrev_name,
+    score_visitor,
+    jornada = matchweek_descriptor
+  ) %>%
   mutate(
     ganador = if_else(score_local > score_visitor, abb_local, abb_visitor)
   ) %>%
@@ -46,12 +49,13 @@ victorias_derrotas <- resultados %>%
     values_to = "abb"
   ) %>%
   mutate(
-    resultado = if_else(abb == ganador, "W", "L")  # W = victoria, L = derrota
+    resultado = if_else(abb == ganador, "W", "L") # W = victoria, L = derrota
   ) %>%
   select(abb, resultado, jornada)
 
-rachas_actual <-victorias_derrotas %>%
-  mutate(jornada = as.numeric(str_remove(jornada, "Jornada "))) %>% 
+# calcular rachas
+rachas_actual <- victorias_derrotas %>%
+  mutate(jornada = as.numeric(str_remove(jornada, "Jornada "))) %>%
   arrange(abb, jornada) %>%
   group_by(abb) %>%
   mutate(
@@ -67,11 +71,9 @@ rachas_actual <-victorias_derrotas %>%
     racha_actual = sum(flag),
     .groups = "drop"
   ) %>%
-  arrange(desc(tipo_racha), desc(racha_actual)) %>% 
-  mutate(win_lose = paste0(tipo_racha, racha_actual)) %>% 
+  arrange(desc(tipo_racha), desc(racha_actual)) %>%
+  mutate(win_lose = paste0(tipo_racha, racha_actual)) %>%
   select(abb, win_lose)
 
-
 # write dataframe to .csv in a folder called "data/"
-#write.csv(rachas_actual, "data/racha_2025_26.csv", row.names = FALSE)
-
+write.csv(rachas_actual, "data/racha_2025_26.csv", row.names = FALSE)

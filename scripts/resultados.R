791dde568e10c218b826682c7ce2a1cc168939ca
Ivan <ivan@Ivan-MacBook-Air.local>
2025-11-09 12:59:39 +0100

    Guarda mis cambios antes del rebase

--- scripts/resultados.R
+++ scripts/resultados.R
@@ -4,40 +4,18 @@ source("scripts/helpers.R")
 if (!dir.exists("data")) dir.create("data")
 
 
-ids <- "https://raw.githubusercontent.com/IvoVillanueva/pbp-acb-2025-26/refs/heads/main/data/id_week.csv"
-
-resids <- GET(url = ids, add_headers(.headers = headers))
-json_resids <- fromJSON(content(resids, "text"))
-ids_df <- json_resids %>%
-  tibble() %>%
-  unnest(cols = c(competition, edition)) |>
-  arrange(num_matchweek)
-
-id <- ids_df$id
+ids <- read_csv("https://raw.githubusercontent.com/IvoVillanueva/pbp-acb-2025-26/refs/heads/main/calendario/ids_df.csv")
 
 
-
-
-# Load match calendar for the 2025-26 ACB season
-calendario <- read_csv(
-  "https://raw.githubusercontent.com/IvoVillanueva/pbp-acb-2025-26/refs/heads/main/calendario/calendario25_26.csv",
-  show_col_types = FALSE
-)
-
-# Filter matches that have already been played
-partidos_2026 <- calendario %>%
-  select(id, matchweek_number, date, time) %>%
+id <- ids %>% 
+  select(id, date = start_date) %>% 
   mutate(
-    date = as.Date(as_datetime(date)),
-    time = hms::hms(time)
+    date = as.Date(as_datetime(date))
   ) %>%
   filter(
-    date < today() |
-      (date == today() & time < hms::as_hms(format(Sys.time(), "%H:%M:%S")))
-  ) %>%
+    date <= today()) %>% 
   pull(id)
 
-id <- 104505
 
 iddf <- function(id) {
   link <- paste0("https://api2.acb.com/api/v1/openapilive/Matches/matchesbymatchweeklite?idCompetition=1&idEdition=90&idMatchweek=", id)

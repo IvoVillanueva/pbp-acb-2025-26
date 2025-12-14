91d582ec1172a77abf25ea0abf884c1e34f755bd
Iván Villanueva Sabalete <villanueva_i1@hotmail.com>
2025-12-14 23:12:03 +0100

    Remove unused pbp_template code
    Removed unused pbp_template code related to JSON processing.

--- scripts/helpers.R
+++ scripts/helpers.R
@@ -12,23 +12,6 @@ headers <- c(
   "x-apikey" = Sys.getenv("ACB_KEY")
 )
 
-
-pbp_template <- fromJSON(content(GET(
-  url = paste0(Sys.getenv("PBP"), partidos_2026[1], "&jvFilter=true"),
-  add_headers(.headers = headers)
-), "text")) %>%
-  pluck() %>%
-  unnest(
-    cols = c(competition, edition, license, team, type, statistics),
-    names_sep = "_"
-  ) %>%
-  select(!c(
-    id_subphase, id_round, license_media, team_media,
-    contains("_date")
-  )) %>%
-  tibble() %>%
-  slice(0)
-
 pbp <- read_csv("https://raw.githubusercontent.com/IvoVillanueva/pbp-acb-2025-26/refs/heads/main/data/playbyplay_2025_26.csv",
                 show_col_types = FALSE) 
  

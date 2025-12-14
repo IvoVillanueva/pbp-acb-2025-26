b73aeb86fdfc8d0149c574e144284fa91e86cd94
Iván Villanueva Sabalete <villanueva_i1@hotmail.com>
2025-12-14 18:16:27 +0100

    Refactor boxscores_matches function for clarity

--- scripts/boxscore_2026.R
+++ scripts/boxscore_2026.R
@@ -28,33 +28,33 @@ partidos_2026 <- calendario %>%
 boxscores_matches <- function(partidos_2026) {
   tryCatch({
     fromJSON(content(GET(
-      url = paste0(
-        Sys.getenv("API_URL"),
-        partidos_2026
-      ),
-      add_headers(.headers = headers)
-    ), "text") %>%
-      pluck() %>%
-      unnest(
-        cols = c(competition, license, local_team, visitor_team, edition),
-        names_sep = "_"
-      ) %>%
-      mutate(
-        abb = ifelse(is_local == FALSE, visitor_team_team_abbrev_name,
-          local_team_team_abbrev_name
-        )
-      ) %>%
-      mutate(
-        fecha = calendario %>%
-          filter(id == partidos_2026) %>%
-          mutate(date = as.Date(as_datetime(date))) %>%
-          pull(date), .before = 1,
-        num_jornada = calendario %>%
-          filter(id == partidos_2026) %>%
-          pull(matchweek_number)
-      )%>%
-      select(where(~ ! is.list(.))) %>%
-      clean_names()
+  url = paste0(
+    Sys.getenv("API_URL"),
+    partidos_2026
+  ),
+  add_headers(.headers = headers)
+), "text")) %>%
+  pluck() %>%
+  unnest(
+    cols = c(competition, license, local_team, visitor_team, edition),
+    names_sep = "_"
+  ) %>%
+  mutate(
+    abb = ifelse(is_local == FALSE, visitor_team_team_abbrev_name,
+      local_team_team_abbrev_name
+    )
+  ) %>%
+  mutate(
+    fecha = calendario %>%
+      filter(id == partidos_2026) %>%
+      mutate(date = as.Date(as_datetime(date))) %>%
+      pull(date), .before = 1,
+    num_jornada = calendario %>%
+      filter(id == partidos_2026) %>%
+      pull(matchweek_number)
+  ) %>%
+  select(where(~ !  is.list(.))) %>%
+  clean_names()
   }, error = function(e) {
     return(NULL)
   })
